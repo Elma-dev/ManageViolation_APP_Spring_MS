dfbdf03321f9b3a92fa069a12ded1b180ac081b2
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-16 12:42:01 +0100

    commit

---
+++ Eureka_Discovery_MS/src/main/java/dev/elma/eureka_discovery_ms/EurekaDiscoveryMsApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.eureka_discovery_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryMsApplication.class, args);
+    }
+
+}
---
+++ Eureka_Discovery_MS/src/test/java/dev/elma/eureka_discovery_ms/EurekaDiscoveryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.eureka_discovery_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
