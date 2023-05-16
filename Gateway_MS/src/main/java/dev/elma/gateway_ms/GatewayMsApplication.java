a8f3d1270131408daf95dd4e870b1a5e2f2aaf9d
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-16 12:32:18 +0100

    commit

--- Gateway_MS/src/main/java/dev/elma/gateway_ms/GatewayMsApplication.java
+++ Gateway_MS/src/main/java/dev/elma/gateway_ms/GatewayMsApplication.java
@@ -2,6 +2,12 @@ package dev.elma.gateway_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayMsApplication {
@@ -9,5 +15,23 @@ public class GatewayMsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayMsApplication.class, args);
     }
+
+    /*
+    @Bean
+    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
+    }
+
+     */
+
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(r->r.path("/owners/**").uri("http://localhost:8081"))
+                .route(r->r.path("/vehicles/**").uri("http://localhost:8081"))
+                .route(r->r.path("/radars/**").uri("http://localhost:8082"))
+                .build();
+    }
+
 
 }
