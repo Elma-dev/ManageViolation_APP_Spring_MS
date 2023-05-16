647c42e2e6ac1bb5fd6f6de355d291f8bcbb0860
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-16 11:35:33 +0100

    commit

--- Immatriculation_MS/src/main/java/dev/elma/immatriculation_ms/ImmatriculationMsApplication.java
+++ Immatriculation_MS/src/main/java/dev/elma/immatriculation_ms/ImmatriculationMsApplication.java
@@ -1,13 +1,40 @@
 package dev.elma.immatriculation_ms;
 
+import dev.elma.immatriculation_ms.entities.Owner;
+import dev.elma.immatriculation_ms.entities.Vehicle;
+import dev.elma.immatriculation_ms.repositories.OwnerRepository;
+import dev.elma.immatriculation_ms.repositories.VehicleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ImmatriculationMsApplication {
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@SpringBootApplication @AllArgsConstructor
+public class ImmatriculationMsApplication implements CommandLineRunner {
+    private OwnerRepository ownerRepository;
+    private VehicleRepository vehicleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ImmatriculationMsApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Stream.of("Ahmed","Mohammed","Abdeljalil").forEach(n->{
+            Owner owner = Owner.builder().name(n).email(n + "@gmail.com").birthDay(new Date()).build();
+            this.ownerRepository.save(owner);
+        });
+        //getOwner
+        Owner abdeljalil = this.ownerRepository.findByName("Abdeljalil").get();
+
+        Stream.of("Ferrari","BMW","RangeRover").forEach(c->{
+            Vehicle vehicle = Vehicle.builder().owner(abdeljalil).model(2023).brand(c).fiscalPower(2.30D).registrationNumber(UUID.randomUUID().toString()).build();
+            vehicleRepository.save(vehicle);
+        });
+    }
 }
--- Immatriculation_MS/src/main/java/dev/elma/immatriculation_ms/web/RestAPI.java
+++ Immatriculation_MS/src/main/java/dev/elma/immatriculation_ms/web/RestAPI.java
@@ -1,23 +1,20 @@
 package dev.elma.immatriculation_ms.web;
 
-import dev.elma.manageviolation_app.entities.Infraction;
-import dev.elma.manageviolation_app.entities.Owner;
-import dev.elma.manageviolation_app.entities.Radar;
-import dev.elma.manageviolation_app.entities.Vehicle;
-import dev.elma.manageviolation_app.repositories.InfractionRepository;
-import dev.elma.manageviolation_app.repositories.OwnerRepository;
-import dev.elma.manageviolation_app.repositories.RadarRepository;
-import dev.elma.manageviolation_app.repositories.VehicleRepository;
+import dev.elma.immatriculation_ms.entities.Owner;
+import dev.elma.immatriculation_ms.entities.Vehicle;
+import dev.elma.immatriculation_ms.repositories.OwnerRepository;
+import dev.elma.immatriculation_ms.repositories.VehicleRepository;
+
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController @AllArgsConstructor
+@RestController
+@AllArgsConstructor
 public class RestAPI {
     private VehicleRepository vehicleRepository;
     private OwnerRepository ownerRepository;
-    private InfractionRepository infractionRepository;
-    private RadarRepository radarRepository;
 
 
     @GetMapping("/owners")
@@ -32,7 +29,7 @@ public class RestAPI {
     }
 
     @PutMapping("/owners/update/{id}")
-    public Owner updateOwner(@PathVariable long id,@RequestBody Owner owner){
+    public Owner updateOwner(@PathVariable long id, @RequestBody Owner owner){
         Owner owner1 = ownerRepository.findById(id).get();
         owner1.setBirthDay(owner.getBirthDay());
         owner1.setName(owner.getName());
@@ -50,15 +47,7 @@ public class RestAPI {
     public List<Vehicle> getAllVehicles(){
         return vehicleRepository.findAll();
     }
-    @GetMapping("/radars")
-    public List<Radar> getAllRadars(){
-        return radarRepository.findAll();
-    }
 
-    @GetMapping("/infractions")
-    public List<Infraction> getAllInfractions(){
-        return infractionRepository.findAll();
-    }
 
 
 }
