9256c87169f767d2488318e2f0e7564c9431b8d2
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-16 12:20:16 +0100

    commit

---
+++ Gateway_MS/src/main/java/dev/elma/gateway_ms/GatewayMsApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.gateway_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMsApplication.class, args);
+    }
+
+}
---
+++ Gateway_MS/src/test/java/dev/elma/gateway_ms/GatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.gateway_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
