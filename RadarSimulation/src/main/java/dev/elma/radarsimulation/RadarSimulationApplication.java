6aae0219ee2ff83e13b64e7178ee6e69fb611191
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-18 15:19:19 +0100

    commit

---
+++ RadarSimulation/src/main/java/dev/elma/radarsimulation/RadarSimulationApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.radarsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadarSimulationApplication.class, args);
+    }
+
+}
---
+++ RadarSimulation/src/test/java/dev/elma/radarsimulation/RadarSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.radarsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
