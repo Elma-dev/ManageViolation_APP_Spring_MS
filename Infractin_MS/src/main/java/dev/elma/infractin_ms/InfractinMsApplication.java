0ae8be7c408f2be674fc2c0c7975cb184c227629
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-19 02:01:05 +0100

    commit

--- Infractin_MS/src/main/java/dev/elma/infractin_ms/InfractinMsApplication.java
+++ Infractin_MS/src/main/java/dev/elma/infractin_ms/InfractinMsApplication.java
@@ -28,18 +28,18 @@ public class InfractinMsApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		List<Vehicle> allVehicle=vehiclesRestClient.getAllVehicles();
-		List<Radar> allRadars = radarsRestClient.getAllRadars();
-
-
-		Vehicle vehicleX = vehiclesRestClient.getVehiclebyRegNumber("98bbbd1d-f4f8-4527-ab11-7cc67482f12a");
-		Radar radar = radarsRestClient.getRadarById(1);
-
-		Infraction infraction = Infraction.builder().idRadr(1)
-				.registrationNumber(vehicleX.getRegistrationNumber())
-				.radarMaxSpeed(radar.getVitessMax()).vehicleSpeed(150).date(new Date())
-				.amount(600D).vehicle(vehicleX).radar(radar).build();
-		infractionRepository.save(infraction);
+//		List<Vehicle> allVehicle=vehiclesRestClient.getAllVehicles();
+//		List<Radar> allRadars = radarsRestClient.getAllRadars();
+//
+//
+//		Vehicle vehicleX = vehiclesRestClient.getVehiclebyRegNumber("98bbbd1d-f4f8-4527-ab11-7cc67482f12a");
+//		Radar radar = radarsRestClient.getRadarById(1);
+//
+//		Infraction infraction = Infraction.builder().idRadr(1)
+//				.registrationNumber(vehicleX.getRegistrationNumber())
+//				.radarMaxSpeed(radar.getVitessMax()).vehicleSpeed(150).date(new Date())
+//				.amount(600D).vehicle(vehicleX).radar(radar).build();
+//		infractionRepository.save(infraction);
 
 		//System.out.println(infraction);
 
